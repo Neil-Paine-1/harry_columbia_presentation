00a9ec6e95cfd9b419cf7fe55970876de3e0fdff
NeilPaine538 <neil.paine@gmail.com>
2016-09-19 23:10:10 -0400

    Add new data/scripting.

--- script.R
+++ script.R
@@ -1,8 +1,37 @@
 #Clean list
 rm(list = ls())
 
+#Load required packages
 require(tidyverse)
 require(clipr)
+require(plotly)
+
+#Exercise time!
+
+#Exercise 1/Sheet 4 - Averaging data
+pollster_averages <- read_csv("pollster_averages.csv")
+
+pollster_averages <- pollster_averages %>% 
+  mutate(clinton_margin_diff=(Clinton_w_3rd-Trump_w_3rd)-(Clinton_wo_3rd-Trump_wo_3rd))
+
+pollster_averages.T = setNames(data.frame(t(pollster_averages[,-1])), as.matrix(pollster_averages[,1]))
+
+pollster_averages.T$Type <- rownames(pollster_averages.T)
+
+pollster_averages.T <- pollster_averages.T %>% 
+  select(5,1:4) %>% 
+  mutate(average=(CNN+Fox_News+Marist+YouGov)/4)
+
+#Exercise 2/Sheet 5 - Plotting data
+states_2012_2016 <- read_csv("states_2012_2016.csv")
+
+states_2012_2016 %>% ggplot(aes(x=`2012`,y=`2016`))+geom_point()
+
+states_2012_2016 %>% ggplot(aes(x=`2012`,y=`2016`))+geom_point()+geom_abline()
+
+#states_2012_2016 %>% plot_ly(x=`2012`,y=`2016`,text=rownames(),mode="markers")
+
+#Excerise 5/Sheet 3 - Doing your own crosstab analysis
 
 trump_conservatives <- read_tsv("trump-conservatives.tsv")
 
